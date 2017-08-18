5c49ce22fd3c6a52fcfc407dbff2d4348864c14c
Myron Anthony <codehearted12@gmail.com>
2017-08-18 13:47:44 -0400

    Todo

---
+++ app.js
@@ -0,0 +1,43 @@
+
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let listOfTodos = [];
+
+  app.get('/', function(request, response) {
+    response.render('todos', { todos: listOfTodos});
+ });
+  app.post ('/', function(request, response) {
+  console.log (request.body)
+
+let id = (Math.random() *1000)
+newTodoObject = { text: request.body.newTodo, id: id}
+listOfTodos.push(newTodoObject);
+
+  console.log(listOfTodos);
+  response.redirect('/');
+
+});
+
+app.post('/mark-complete/:id')
+let idOfTheTodoThatImMarkingComplete = parseInt (request.params.id);
+let completedTodo = listOfTodos.find(function (todo) {
+  return todo.id === idOfTheTodoThatImMarkingComplete
+})
+competedTodo.compete = true;
+response.redirect('/');
+
+ app.listen(3000, function () {
+   console.log("Hello")
+ });
